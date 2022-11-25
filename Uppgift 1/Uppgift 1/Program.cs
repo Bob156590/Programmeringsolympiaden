542596df4c140fe7011df096b7e483b8bee9efec
Bob156590 <abad04002@utb.vaxjo.se>
2022-11-25 12:05:25 +0100

    yaya

---
+++ Uppgift 1/Uppgift 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace tba
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int tor = 0;
+            int mor = 0;
+            int morötter = 40;
+            bool kk = true;
+            try
+            {
+                Console.WriteLine("Tors tid ? ");
+                int tort = int.Parse(Console.ReadLine());
+                Console.WriteLine("Mors tid ? ");
+                int mort = int.Parse(Console.ReadLine());
+                for (i = 0; kk; i++)
+                {
+                    if (morötter == 1 && i % tort == 0 && i % mort == 0)
+                    {
+                        kk = false; 
+                    }
+                    else if (i % tort == 0 && morötter != 0)
+                    {
+                        morötter--;
+                        tor++;
+                    }
+                    if (morötter == 1 && i % tort == 0 && i % mort == 0)
+                    {
+                        kk = false;
+                    }
+                    else if (i % mort == 0 && morötter != 0)
+                    {
+                        morötter--;
+                        mor++;
+                    }
+                }
+               
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Pls write only numbers");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You can't duvude by zero dumbass.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+
+            Console.WriteLine($"SVAR:Tor {tor},     Mor {mor}");
+        }
+    }
+}
---
+++ uppgift 4/uppgift 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace tba
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Antal ettor ? ");
+            int ettt =  int.Parse(Console.ReadLine());
+            Console.Write("Antal tvåor ? ");
+            int tvåå = int.Parse(Console.ReadLine());
+            Console.Write("Antal treor ? ");
+            int tree = int.Parse(Console.ReadLine());
+            Console.Write("Antal fyror ? ");
+            int fyraa = int.Parse(Console.ReadLine());
+
+            int ett = ettt;
+            int två = 2*tvåå;
+            int tre = 3*tree;
+            int fyra = 4*fyraa;
+
+            int lag = (ett+två+fyra+tre)/2;
+            ett = 0;
+            två = 0;
+            tre = 0;
+            fyra = 0;
+            while (lag > 0)
+            {
+                if (fyraa > fyra && lag >= 4 && fyraa != 0)
+                {
+                        lag -= 4;
+                        fyra++;
+                }
+                else if (tree > tre && lag >= 3 && tree != 0)
+                {
+                        lag -= 3;
+                        tre++;
+                }
+                else if (tvåå > två && lag >=2 && tvåå != 0)
+                {
+                        lag -= 2;
+                        två++;
+                }
+                else if (ettt > ett && lag >= 1 && ettt != 0)
+                {
+                        lag -= 1;
+                        ett++;
+                }
+            }
+            Console.WriteLine($"Svar: {ett} {två} {tre} {fyra}");
+
+        }
+    }
+}
